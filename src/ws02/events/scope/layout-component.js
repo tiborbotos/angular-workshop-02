04164a2afc4d1a3eca107357a3be0f11c01057a5
evelinhlacsok <evelinhlacsok@gmail.com>
2019-07-23 14:43:42 +0200

    add and delete works

--- src/ws02/events/scope/layout-component.js
+++ src/ws02/events/scope/layout-component.js
@@ -5,4 +5,16 @@ export default class LayoutComponent {
       controller: LayoutComponent,
     };
   }
+
+  newTodo = "water the plant";
+  todoList = ['walk the dog', 'shopping'];
+
+  onAdd(){
+    this.todoList.push(this.newTodo);
+    this.newTodo = "";
+  }
+
+  onDelete(index){
+    this.todoList.splice(index, 1);
+  }
 }
