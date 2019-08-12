7bdd286d31b3dc5a523784b4cabc3b509c97843b
evelinhlacsok <evelinhlacsok@gmail.com>
2019-08-12 10:55:45 +0200

    add and delete drink works

--- src/ordering-item/ordering-item.js
+++ src/ordering-item/ordering-item.js
@@ -6,6 +6,23 @@ export default class OrderingItemComponent {
     };
   }
 
-  newDrink = 'almafroccs';
+  newDrink = "";
   drinkList = ['szoda', 'limonade', 'rum'];
+
+  onAdd() {
+    if (this.newDrink.length > 0) {
+      this.drinkList.push(this.newDrink);
+      this.newDrink = "";
+    }
+  }
+
+  onEnter(event){
+  if (event.which === 13){
+    this.onAdd();
+    }
+  }
+
+  onDelete(index) {
+    this.drinkList.splice(index, 1);
+  }
 }
